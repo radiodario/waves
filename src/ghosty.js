70b0452486ab277c766e59fa434904024899a400
radiodario <dario.villanueva@gmail.com>
2014-02-18 00:48:21 +0000

    i like waves

--- src/ghosty.js
+++ src/ghosty.js
@@ -49,7 +49,7 @@ function calculateX() {
 function calculateY() {
   var angle = mapR(frameCount, 0, height, 0, 2*Math.PI);
   
-  return height/2 - (Math.cos(angle)*height/3)
+  return height/2 - (Math.sin(angle)*height/3)
 }
 
 
@@ -63,13 +63,13 @@ function fade() {
 var particleSystem = function() {
  
   var particles = [];
-  var max_particles = 10000;
+  var max_particles = 1000;
   
   return {
     drawParticles : function() {
       ctx.beginPath();
 
-      w = 10;
+      w = 2;
 
       var r, g, b;
 
@@ -77,9 +77,6 @@ var particleSystem = function() {
         if (i == 0) {}
           ctx.moveTo(particles[i][0], particles[i][1]);
         
-        
-
-
         ctx.lineTo(
           particles[i][0]+mapR(Math.random(), 0, 1, -w, w), 
           particles[i][1]+mapR(Math.random(), 0, 1, -w, w)
@@ -109,7 +106,7 @@ var particleSystem = function() {
     
     updateParticles: function() {
       if (particles.length < max_particles) {
-        this.addParticle(frameCount%width,calculateY());
+        this.addParticle(width,calculateY());
       }
       for (var i = 0; i < particles.length; i++) {
         if (particles[i][0]>= 0) {
@@ -135,6 +132,9 @@ system.updateParticles();
 
 var opacity = 0;
 
+var sweep = 1;
+var increase = 0;
+
 
 function animate() {
   fade();
@@ -148,9 +148,10 @@ function animate() {
   system.drawParticles();
   system.updateParticles();
 
- 
   
-  frameCount+= w;
+
+  
+  frameCount += mouseX;
 
 
   var x = calculateX();
